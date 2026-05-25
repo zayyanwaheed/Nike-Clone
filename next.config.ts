3ebcd45be7b3237807650e1c0134cbe953e8b17b
ZENOX <zayyanwaheed098@gmail.com>
2026-05-25 22:59:03 +0500

    Disable Next dev indicator

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  devIndicators: false,
   reactStrictMode: false,
 };
 
